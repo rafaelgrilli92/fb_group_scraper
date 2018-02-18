c729880b9834b27e1b205ac28bea0501c1237c97
Rafael Grilli <rafaelgrilli92@hotmail.com>
2018-02-19 02:43:09 +1100

    process names and removing watch

--- pm2.ecosystem.config.js
+++ pm2.ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
 	apps: [
 		{
-			name: "scraper_checker",
+			name: "checker",
 			cwd: "/home/ubuntu/fb_group_scraper/",
 			script: "/home/ubuntu/fb_group_scraper/server/index.js",
-			watch: true,
+			watch: false,
 			env: {
 				"NODE_ENV": "production"
 			}
 		}, {
-			name: "fb_group_scraper",
+			name: "scraper",
 			cwd: "/home/ubuntu/fb_group_scraper/",
 			script: "/home/ubuntu/fb_group_scraper/scraper.js",
 			interpreter: "/usr/lib/node_modules/casperjs/bin/casperjs.js",
-			watch: true,
+			watch: false,
 			env: {
 				"NODE_ENV": "production"
 			}
